d2cb8da3a809e553917f827aba9dbf4fd922bd41
Adminl <lst.thierry@gmail.com>
2018-08-03 15:34:31 +0200

    HelloWord avec commentaire

--- Test/HelloWord/HelloWord/Program.cs
+++ Test/HelloWord/HelloWord/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Première application "Hello Word !"
+Utilisation des Methodes ;
+  Console.WriteLine(ce que l'on souhaite afficher) : permet l'affichage du résultat demandé à la console
+  Console.Readkey(...) & Console.ReadLine(...)     : permet à la console de recuperer 'la ligne || la touche' tapé par l'utilsateur
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +13,15 @@ using System.Threading.Tasks;
 namespace HelloWord
 {
     class Program
-    {
+    {//la méthode Main est le point d'entré du programme, dans la class Program
         static void Main(string[] args)
         {
+            //Console.WriteLine("donnés à afficher")
+            //L'éxecution de l'instruction ne se fera uniquement si l'on ajoute le ';' autrement rien
+            Console.WriteLine("Hello Word!");
+
+            //Console.ReadKey("donnés que récupère la cosole")
+            Console.ReadKey();
         }
     }
 }
