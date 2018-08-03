4a543079e08ebf5a47941bdd7829c662cac86834
Adminl <lst.thierry@gmail.com>
2018-08-03 15:30:07 +0200

    Application TVA

--- Test/TVA/TVA/Properties/AssemblyInfo.cs
+++ Test/TVA/TVA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("TVA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("TVA")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("53941df9-0689-4999-b389-fb6c42590df0")]
+[assembly: Guid("23c75003-d1df-48b7-be8e-325aeda00bc3")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
