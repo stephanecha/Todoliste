14bbfaf3c282c5071f2a3025229dc37ec3b83c5a
Adminl <lst.thierry@gmail.com>
2018-08-03 15:38:54 +0200

    travail sur les listes

---
+++ Test/Mail/Mail/Program.cs
@@ -0,0 +1,59 @@
+﻿//Diag de classe UML, voir les acces et relations induites
+
+
+
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.IO;
+
+
+
+namespace Mail
+
+{
+
+    public class Email
+
+    {
+
+        public string Sujet { get; set; }
+
+        public string Destinataire { get; set; }
+
+        public string Contenu { get; set; }
+
+        public string[] DestinatairesEnCopie { get; set; }
+
+
+
+        public List<PieceJointe> PiecesJointe { get; set; } //liste infini
+
+        public PieceJointe PieceJointe { get; set; } // pas de liste
+
+    }
+
+
+
+    public class PieceJointe
+
+    {
+
+        public Email { get; set; }
+
+    public DateTime DateAjout { get; set; }
+
+    public FileInfo fileInfo { get; set; }
+
+}
+
+
+
+}
