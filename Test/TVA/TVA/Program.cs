d8b7b9ae805c7209c79b27765cd84eabf0ece9c1
Eternal-Ben <Eternalst@live.fr>
2018-08-05 21:07:05 +0200

    Calcul simple d'un taux donné (TVA) avec affichage du surcout et du montant ttc.

--- Test/TVA/TVA/Program.cs
+++ Test/TVA/TVA/Program.cs
@@ -1,14 +1,18 @@
-﻿/*Calculer la tva d'un produit ht et en donner son resultat.
+﻿/*
+Calculer la tva d'un produit ht et en donner son resultat.
 Demander à l'utilisateur d'introduire un montant hors tva, ainsi que son taux de tva
 
- 
+le programme doit calculer ET afficher la TVA à payer en euros ET le montant total TVA.
 
-le programme doit calculer ET afficher la TVA a payer en euros et le montant total TVA.
-
+Ebauche du programme :
+             * Veuillez nous préciser le montant HT de votre produit ainsi que votre taux de Tva ? :
+             * HT
+             * Taux tva
+             * Récap de vos frais :
+             * taxe
+             * TtC
+             * Le montant total est de xx€ TTC
 */
-
-
-
 using System;
 
 using System.Collections.Generic;
@@ -32,44 +36,28 @@ namespace TVA
         static void Main(string[] args)
 
         {
+            string coutHt = "Veuillez préciser le montant Ht de votre produit";
+            string tauxTva = "\nLe taux de vôtre Tva local étant de";
+            string message = "\nLe montant Ttc de votre produit sera de ";
+            string detailOperation = "\nDétail de vos opérations :";
 
-            double tauxTva = 1.206;
+            var c = 100;
+            double taxe = 20.6;
 
+            Console.WriteLine(coutHt);
+            var ht = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine(tauxTva+" "+taxe+" %");
 
-            Console.WriteLine("Quel est le Montant HT de votre article :");
+            double prixTtc = (ht + (ht * taxe / c));
+            double fTva = prixTtc - ht;
 
-            Console.ReadLine();
+            Console.WriteLine(message+prixTtc+" euros");
 
-            Console.WriteLine("\nVeuillez préciser votre taux TVA local :");
-
-            Console.ReadLine();
-
-
-
-            Console.WriteLine("\nLe coût de la TVA à payer est de :");
-
-
-
-            Console.WriteLine("\nLe prix TTC de votre article sera de :");
-
-        }
-
-        private static void CoutTva(double())
-
-        {
-
-            // coût tva à payer = (montant ht + Tva)- montant ht            
+            Console.WriteLine(detailOperation+ "\nTva : "+fTva+" euros"+"\nCout Ht : " + ht+" euros");
+            Console.WriteLine("\nFin de l'opération, à bientôt.\n");
 
         }
-
-        static void CoutTtc()
-
-        {
-
-
-
-        }----
 
     }
 
