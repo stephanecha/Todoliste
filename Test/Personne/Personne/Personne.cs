9766607f206c76f6be32adcf8c767cd341b5d205
Eternal-Ben <Eternalst@live.fr>
2018-08-09 02:15:53 +0200

    Creation d'une classe Personne, comporte nom, prenom, age.

---
+++ Test/Personne/Personne/Personne.cs
@@ -0,0 +1,19 @@
+﻿/*Ecrire une classe personne qui contiendra les variable nom, prenom, et age
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personne
+{
+         public class Personne
+
+        {
+            public string nom;
+            public string prenom;
+            public int age;
+           
+        }
+}
