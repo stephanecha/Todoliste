1784de2bb74c62709a68c09f1052876399b25e66
Eternal-Ben <Eternalst@live.fr>
2018-08-28 20:50:43 +0200

    Le HelloWord c'est muté en une sorte de programme de mot de passe simple. comme User "Alex" et mdp " UserCom" de là on accede a un menu tres simple mais sans acces possible..., si mauvais mdp ou  user, il n'y a pas de possibilité d'acces au menu.

--- Test/HelloWord/HelloWord/Program.cs
+++ Test/HelloWord/HelloWord/Program.cs
@@ -17,42 +17,60 @@ namespace HelloWord
         static void Main(string[] args)
         {
 
-            Console.WriteLine("\nWhat is your name ?");
+            Console.WriteLine("\nVotre identifiant qui est votre prenom, je vous pries");
             var name = Console.ReadLine();
             var date = DateTime.Now;
 
-            Console.WriteLine("\nQue cherches tu ?");
-            var chercher = Console.ReadLine();
-
-            Console.WriteLine("\nVraiment ?");
-            Console.ReadLine();
-
-            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t} tu cherches {chercher}!");
-
-            string yBoss = "Hey Jhonny";
-            string unBadasse = "Ok Badasse";
-            yBoss = yBoss.Replace("Hey", "La vache");
-            yBoss = yBoss.Remove(0);
-
-            Console.WriteLine($"{yBoss}, wait wait wait just a check, {unBadasse}");
-            int Verre = 0;
-            while (Verre < 10)
+            if (name != "Alex")//une liste de commercial par exemple
             {
-                Console.WriteLine($"Je te fabrique, {Verre}, verres. ");
-                Verre++;
+                Console.WriteLine("\n\aVous n'avez pas acces, désolé !");
+                Console.ReadKey();
             }
- //           int Verre = 0;
-            do
+            else
             {
-                Console.WriteLine($"combien en reste t il? {Verre}");
-                Verre--;
-            } while (Verre<20)
-
-            Console.WriteLine("\nPress any key to exit...");
-            Console.ReadKey(true);
-
-            // le merge à fonctionné dans le sens Iteration 2 au master
-
+                Console.WriteLine($"\nBienvenu {name}, veuillez entrez votre mot de passe");
+                var mdp = Console.ReadLine();
+                if (mdp == "UserCom")//liste de mdp egalement...
+                {
+                    Console.WriteLine($"\n\n\beepQue souhaitez vous réaliser aujourd'hui {name}?");
+                    Console.WriteLine("1. Visualiser :"); // faire une liste de consultation de client et de voyage en mode sql
+                    Console.WriteLine("2. Dénicher :"); // idem ci dessus liste de voyage en dispo avec liste de client acheteur
+                    Console.WriteLine("3. Prospecter :"); // liste client potentiel et voyage dispo avec promo enfant 12 ans et liste tranche d'age etc via sql ?
+                    Console.WriteLine("Q : Revenir un peux plus tard finalement...");
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
+//            Console.ReadLine();
+
+//            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t} tu cherches {chercher}!");
+
+//            string yBoss = "Hey Jhonny";
+//            string unBadasse = "Ok Badasse";
+//            yBoss = yBoss.Replace("Hey", "La vache");
+//            yBoss = yBoss.Remove(0);
+
+//            Console.WriteLine($"{yBoss}, wait wait wait just a check, {unBadasse}");
+//            int Verre = 0;
+//            while (Verre < 10)
+//            {
+//                Console.WriteLine($"Je te fabrique, {Verre}, verres. ");
+//                Verre++;
+//            }
+// //           int Verre = 0;
+//            do
+//            {
+//                Console.WriteLine($"combien en reste t il? {Verre}");
+//                Verre--;
+//            } while (Verre<20)
+
+//            Console.WriteLine("\nPress any key to exit...");
+//            Console.ReadKey(true);
+
+//            // le merge à fonctionné dans le sens Iteration 2 au master
+
+//        }
+//    }
+//}
