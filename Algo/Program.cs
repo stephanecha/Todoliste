b4d92ac1f77173b7f80e46b52b652fab1bbf80ed
jsurville <julien.surville@gmail.com>
2018-09-12 19:04:02 +0200

    T sur Algo et boucle For

---
+++ Algo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Merci de rentrer votre note");
+			int n = int.Parse(Console.ReadLine()); // Il s'agit de int Parse pour la convertion du string en int.
+			int sommes = 0;
+			int moyenne = 0;
+
+			for (int i=0; i<n; i++) // for = boucle, une structure iterative | revoir boucle
+			{
+				int x = int.Parse(Console.ReadLine());
+				sommes = sommes + x;
+			}
+
+				moyenne = sommes / n;
+			Console.WriteLine($"ta mayonne{moyenne}");
+			Console.ReadKey(true);
+		}
+	} 
+}
