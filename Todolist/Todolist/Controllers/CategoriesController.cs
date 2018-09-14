5e014984a8b2cce50ef9e5cd6ac2012780f13c8c
stephanecha <schamblain@gmail.com>
2018-09-14 16:38:59 +0200

    mis a jour categ controleur

--- Todolist/Todolist/Controllers/CategoriesController.cs
+++ Todolist/Todolist/Controllers/CategoriesController.cs
@@ -20,20 +20,97 @@ namespace Todolist.Controllers
 
 		}
 
+		public IHttpActionResult GetCategorie(int id)
+		{
+			var categorie = db.Categories.Find(id);
+			if (categorie == null)
+				return NotFound();
+			return Ok(categorie);
+		}
+
 		public IHttpActionResult PostCategories(Categorie categorie)
 		{
-			db.Categories.Add(categorie);
+			if (ModelState.IsValid)
+			{
+				db.Categories.Add(categorie);
+				db.SaveChanges();
+				return Ok(categorie);
+
+			}
+			else
+				return BadRequest(ModelState);
+		}
+		public IHttpActionResult PutCategories(int id, Categorie categorie)
+		{
+			if (id != categorie.ID)
+				return BadRequest();
+
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			if(db.Categories.Count(x=> x.ID == id)!=1)
+				return BadRequest();
+
+			db.Entry(categorie).State = System.Data.Entity.EntityState.Modified;
+			db.SaveChanges();
+
+			return StatusCode(HttpStatusCode.NoContent);
+
+			
+
+
+		}
+
+
+		/*public IHttpActionResult DeleteCategoriesOLD(int id, Categorie categorie)
+		{
+			if (id != categorie.ID)
+				return BadRequest();
+
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			if (db.Categories.Count(x => x.ID == id) != 1)
+				return BadRequest();
+
+			db.Entry(categorie).State = System.Data.Entity.EntityState.Deleted;
+			db.SaveChanges();
+
+			return StatusCode(HttpStatusCode.NoContent);
+
+
+
+
+		}*/
+
+		public IHttpActionResult DeleteCategories(int id)
+		{
+			var categorie = db.Categories.Find(id);
+
+			if (categorie==null)
+				return NotFound();
+
+			
+
+			if (db.Categories.Count(x => x.ID == id) != 1)
+				return BadRequest();
+
+			db.Entry(categorie).State = System.Data.Entity.EntityState.Deleted;
 			db.SaveChanges();
 
 			return Ok(categorie);
+
+
+
+
 		}
 		protected override void Dispose(bool disposing)
 		{
 			base.Dispose(disposing);
 			db.Dispose();
 		}
-		
-			
-		
+
+
+
 	}
 }
