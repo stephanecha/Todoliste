adfc9932462a78b9db43a6da2715989386a7656c
jsurville <julien.surville@gmail.com>
2018-09-14 10:56:20 +0200

    Configuration Migration

---
+++ Todolist/Todolist/Migrations/201809140842379_Init.cs
@@ -0,0 +1,26 @@
+namespace Todolist.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.categories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Nom = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.categories");
+        }
+    }
+}
---
+++ Todolist/Todolist/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Migrations;
+using Todolist.Data;
+
+namespace Todolist.Migrations
+{
+	public class Configuration :DbMigrationsConfiguration<TodoDbContext>
+	{
+		public Configuration ()
+		{
+
+		
+
+		AutomaticMigrationsEnabled = false;
+		}
+	}
+}
