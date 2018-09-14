e2ea2f2a314fc960213b4a5d37a74b88b126d3be
stephanecha <schamblain@gmail.com>
2018-09-14 12:48:15 +0200

    todo

---
+++ Todolist/Todolist/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Todolist.Data;
+using Todolist.Models;
+
+
+namespace Todolist.Controllers
+{
+	public class CategoriesController : ApiController
+	{
+		private TodoDbContext db = new TodoDbContext();
+
+		public IQueryable<categorie> GetCategories()
+		{
+			return db.Categories;
+
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			db.Dispose();
+		}
+		
+			
+		
+	}
+}
--- Todolist/Todolist/Models/categorie.cs
+++ Todolist/Todolist/Models/categorie.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Todolist.Models
 {
 	public class categorie
 	{
+
 		public int ID { get; set; }
+		[Required]
+		[StringLength(20)]
 		public string Nom { get; set; }
 	}
 }
